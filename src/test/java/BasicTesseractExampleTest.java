084ca7613c016851d83c80ac7aaf8e7db36e66fe
nirojans <nirojan.selvanathan@trycake.com>
2017-01-04 18:38:06 +0530

    adding the config files

--- src/test/java/BasicTesseractExampleTest.java
+++ src/test/java/BasicTesseractExampleTest.java
@@ -1,6 +1,12 @@
 import org.bytedeco.javacpp.*;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
 import static org.bytedeco.javacpp.lept.*;
 import static org.bytedeco.javacpp.tesseract.*;
 import static org.junit.Assert.assertTrue;
@@ -31,5 +37,39 @@ public class BasicTesseractExampleTest {
         api.End();
         outText.deallocate();
         pixDestroy(image);
+
+
+        String resourceName = "config.properties"; // could also be a constant
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        Properties props = new Properties();
+        try (InputStream resourceStream = loader.getResourceAsStream(resourceName)) {
+            props.load(resourceStream);
+            System.out.print(props.toString());
+        } catch (Exception e) {
+
+        }
+    }
+
+    public static String processReceipts() throws IOException, InterruptedException{
+
+        StringBuffer output = new StringBuffer();
+
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec("echo 'hello world'");
+            p.waitFor();
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+        } catch (InterruptedException e) {
+            throw e;
+        } catch (IOException e) {
+            throw e;
+        }
+        return output.toString();
     }
 }
